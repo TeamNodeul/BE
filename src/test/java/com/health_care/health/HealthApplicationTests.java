b5a21289657c85077d98b40eb7b83a7e27257f81
seulhuioh <seulhuioh0710@gmail.com>
2025-01-05 01:20:36 +0900

    feat- initialize docker

---
+++ src/test/java/com/health_care/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health_care.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
